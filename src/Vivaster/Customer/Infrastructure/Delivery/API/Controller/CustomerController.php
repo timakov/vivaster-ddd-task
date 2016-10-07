9df1976972b11e653fb79e03b743736e92d36544
Sergey Timakov <mortiis.aka.morbid@gmail.com>
2016-10-07 19:25:16 +0300

    Using DIC

--- src/Vivaster/Customer/Infrastructure/Delivery/API/Controller/CustomerController.php
+++ src/Vivaster/Customer/Infrastructure/Delivery/API/Controller/CustomerController.php
@@ -2,6 +2,8 @@
 
 namespace Vivaster\Customer\Infrastructure\Delivery\API\Controller;
 
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Symfony\Component\HttpFoundation\Response;
 use Vivaster\Customer\Infrastructure\Persistence\InMemoryCustomerRepository;
 use Vivaster\Customer\Application\ViewCustomerService;
@@ -10,17 +12,14 @@ use Vivaster\Customer\Application\UpdateCustomerService;
 use Vivaster\Customer\Application\UpdateCustomerRequest;
 use Vivaster\Customer\Application\DataTransformer\CustomerJsonDataTransformer;
 
-final class CustomerController
+final class CustomerController implements ContainerAwareInterface
 {
+    use ContainerAwareTrait;
+
     public function getAction($customerId)
     {
         return new Response(
-            (
-                new ViewCustomerService(
-                    new InMemoryCustomerRepository(),
-                    new CustomerJsonDataTransformer()
-                )
-            )->execute(
+            $this->container->get('customer.app.action.view')->execute(
                 new ViewCustomerRequest($customerId)
             )
         );
@@ -31,12 +30,7 @@ final class CustomerController
         parse_str(file_get_contents('php://input'), $data);
 
         return new Response(
-            (
-                new UpdateCustomerService(
-                    new InMemoryCustomerRepository(),
-                    new CustomerJsonDataTransformer()
-                )
-            )->execute(
+            $this->container->get('customer.app.action.update')->execute(
                 new UpdateCustomerRequest($customerId, $data)
             )
         );
